87996010c5ea0934ede89b41d66f42b13b835f06
William Chrapcynski <wchrapcynski@gmail.com>
2019-11-22 14:18:47 -0500

    Initial Upload

---
+++ test/Add.test.js
@@ -0,0 +1,17 @@
+import Add from '../src/components/Add';
+import Form from '../src/components/Form';
+let wrapper;
+beforeEach(() => {
+    wrapper = shallow(<Add />);
+});
+describe('<Add /> rendering', () => {
+    it('should render one <h1>', () => {
+        expect(wrapper.find('h1')).toHaveLength(1);
+    });
+it('should render one <Form>', () => {
+        expect(wrapper.find(Form)).toHaveLength(1);
+    });
+it('should render 2 <label>s', () => {
+        expect(wrapper.find('label')).toHaveLength(2);
+    });
+});
